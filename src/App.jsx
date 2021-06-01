8674c3d6453a1ce3bdc490b885de6e4005049fc1
Eric <rc84.eric@gmail.com>
2021-06-01 22:08:01 +0200

    routing & bootstrap stylin

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Header from './components/Header';
+import CoinSummaryPage from './pages/CoinSummaryPage';
+import "./App.css";
+
+const App = () => {
+    return (
+        <div>
+        <BrowserRouter>
+         <Header />
+          <Route exact path="/" component={CoinSummaryPage} />
+        </BrowserRouter>
+        </div>
+    )
+}
+
+export default App
