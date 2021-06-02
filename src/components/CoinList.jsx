88ced407224b308e14dde92e5399bbd0fdd32b25
Eric <rc84.eric@gmail.com>
2021-06-02 21:06:46 +0200

    coingeko baseURL

--- src/components/CoinList.jsx
+++ src/components/CoinList.jsx
@@ -1,12 +1,18 @@
-import React, { useEffect, useState } from 'react':
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import coinGeko from '../apis/coinGecko';
 
 const CoinList = () => {
-    const [coins, setCoins] = useState([])
+    // const [coins, setCoins] = useState([]);
 
     useEffect(() => {
-      const fetchData = () => {
-
+      const fetchData = async() => {
+        const response = await coinGeko.get('/coins/markets', {
+          params: {
+            vs_currency: "usd",
+            ids: "bitcoin,ethereum"
+          }
+        })
+        console.log(response.data);
       }
 
       fetchData()  
