adf6c08149e215941df4052221f379470fb9499e
Eric <rc84.eric@gmail.com>
2021-06-03 20:22:57 +0200

    url params

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
 import Header from './components/Header';
 import CoinSummaryPage from './pages/CoinSummaryPage';
+import CoinDetailPage from './pages/CoinDetailPage';
 import "./App.css";
 import { WatchListContextProvider } from './context/watchListContext';
 
@@ -12,6 +13,7 @@ const App = () => {
             <BrowserRouter>
             <Header />
             <Route exact path="/" component={CoinSummaryPage} />
+            <Route path="/coins/:id" component={CoinDetailPage} />
             </BrowserRouter>
         </WatchListContextProvider>
         </div>
