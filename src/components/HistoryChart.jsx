d0347bd7586d9f489134b7eb4136fe8ca22315a0
Eric <rc84.eric@gmail.com>
2021-06-03 22:15:16 +0200

    charts

--- src/components/HistoryChart.jsx
+++ src/components/HistoryChart.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import Chartjs from 'chartjs';
 
 const HistoryChart = () => {
+    const chartRef = useRef()
+
+    useEffect(() => {
+     if (chartRef && chartRef.current) {
+        const chartInstance = new Chartjs() 
+     } 
+    })
     return (
         <div>
-            
+          <canvas ref={chartRef} id="myChart" width={250} height={250}></canvas>  
         </div>
     )
 }
